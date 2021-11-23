d1bc427f428e737376e1af1e828ea5619a1c825f
Vikas Gavandi <30316401+DIGILABTEAM@users.noreply.github.com>
2021-11-23 11:15:34 +0530

    project deployment

---
+++ js/questions.js
@@ -0,0 +1,46 @@
+/*All questions in the quiz goes here in the data object.....*/
+var data =
+	[
+		[
+			{
+				question: "Unequal consolidation on a CXR can be caused by all except:",
+				option1: "Pulmonary oedema",
+				option2: "Pneumonia",
+				option3: "Pulmonary haemorrhage",
+				option4: "Pulmonary infarction",
+				answer: 1
+			},
+			{
+				question: "Physiological consequences of obstructive sleep apnoea usually include each of the following EXCEPT",
+				option1: "Stimulation of erythropoiesis",
+				option2: "Pulmonary vasoconstriction",
+				option3: "Tachycardia followed by bradycardia",
+				option4: "Systemic vasoconstriction",
+				answer: 3
+			},
+			{
+				question: "In chronic obstructive pulmonary disease (COPD), the variable most closely associated with prognosis is",
+				option1: "Arterial carbon dioxide partial pressure (PaC02)",
+				option2: "Arterial oxygen partial pressure (Pa02)",
+				option3: "Forced expiratory volume in one second (FEV1)",
+				option4: "Forced vital capacity (FVC)",
+				answer: 1
+			},
+			{
+				question: "The most useful clinical finding to exclude the presence of airflow limitation in a 60-year-old is",
+				option1: "History of never smoking",
+				option2: "Absence of hyperresonance to chest percussion",
+				option3: "Normal cardiac dullness to percussion",
+				option4: "Absence of pulsus paradoxus",
+				answer: 1
+			},
+			{
+				question: "The lobar collapse which is most easily missed on chest X-ray, is",
+				option1: "Right upper lobe",
+				option2: "Left lower lobe",
+				option3: "Right lower lobe",
+				option4: "Left upper lobe",
+				answer: 4
+			}
+		]
+	];
